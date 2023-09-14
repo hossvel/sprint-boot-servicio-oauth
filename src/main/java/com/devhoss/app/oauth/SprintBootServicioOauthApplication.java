0d6381f1df6d83e1bff2a6efa3d5de60ced3ca2b
devhoss <velasco2288@gmail.com>
2023-09-14 00:02:19 -0500

    first commit

---
+++ src/main/java/com/devhoss/app/oauth/SprintBootServicioOauthApplication.java
@@ -0,0 +1,13 @@
+package com.devhoss.app.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootServicioOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBootServicioOauthApplication.class, args);
+	}
+
+}
